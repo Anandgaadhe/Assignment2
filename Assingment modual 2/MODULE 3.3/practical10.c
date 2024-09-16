f90415c43ab7060a6f15f35a3826a8cd408805d5
Anandgaadhe <anandgaadhe368@gmail.com>
2024-09-16 12:56:33 +0530

    Update practical10.c

--- Assingment modual 2/MODULE 3.3/practical10.c
+++ Assingment modual 2/MODULE 3.3/practical10.c
@@ -1,4 +1,4 @@
-//10. Write a program of structure for five employee that provides the following information -print and display empno, empname, address and age */
+//10. Write a program of structure for five employee that provides the following information -print and display empno, empname, address and age 
 #include<stdio.h>
 struct employe
 {
